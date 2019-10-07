d73cb3f6886714d1fbecc60cb9a9e22db86632bc
Zia Fazal <zia.fazal@arbisoft.com>
2019-10-07 16:59:15 +0500

    Show blank screen instead of logout message for Enterprise SSO flow
    Changes to show blank screen instead of logout message for Enterprise SSO flow. It confuses learner when they are already signed in and access their course from B2B site.
    ENT-1688
    
    Fixed pep8 quality violation
    
    use testserver as HOST instead of edx.org
    
    assert enterprise app is enabled
    
    added logging
    
    Print different values
    
    Reload django url config
    
    assert all quote operations to see how they work on jenkins
    
    debug url resolved values
    
    login user in setup
    
    added log to debug test on jenkins
    
    Using UrlResetMixin
    
    Using regex instead of resolve
    
    Fixed quality violation

--- openedx/core/djangoapps/user_authn/views/logout.py
+++ openedx/core/djangoapps/user_authn/views/logout.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import
 
 import edx_oauth2_provider
+import logging
+import re
 import six.moves.urllib.parse as parse  # pylint: disable=import-error
 from django.conf import settings
 from django.contrib.auth import logout
@@ -13,6 +15,8 @@ from six.moves.urllib.parse import parse_qs, urlsplit, urlunsplit  # pylint: dis
 
 from openedx.core.djangoapps.user_authn.cookies import delete_logged_in_cookies
 from openedx.core.djangoapps.user_authn.utils import is_safe_login_or_logout_redirect
+
+log = logging.getLogger(__name__)
 
 
 class LogoutView(TemplateView):
@@ -98,6 +102,18 @@ class LogoutView(TemplateView):
         new_query_string = urlencode(query_params, doseq=True)
         return urlunsplit((scheme, netloc, path, new_query_string, fragment))
 
+    def _is_enterprise_target(self, url):
+        """
+        Check if url belongs to enterprise app
+
+        Args: url(str): url path
+        """
+        url = parse.unquote_plus(parse.quote(url))
+        if re.match(r'^/enterprise/[a-z0-9\-]+/course', url):
+            return True
+        else:
+            return False
+
     def get_context_data(self, **kwargs):
         context = super(LogoutView, self).get_context_data(**kwargs)
 
@@ -122,9 +138,11 @@ class LogoutView(TemplateView):
             if not referrer or (referrer and not uri.startswith(referrer)):
                 logout_uris.append(self._build_logout_url(uri))
 
+        target = self.target
         context.update({
-            'target': self.target,
+            'target': target,
             'logout_uris': logout_uris,
+            'enterprise_target': self._is_enterprise_target(target),
         })
 
         return context
